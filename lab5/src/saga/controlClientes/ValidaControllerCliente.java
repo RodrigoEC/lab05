9ca9d75f4b14a801f9d8821800aa45a7b8358d4e
Rodrigo Eloy <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2019-05-13 19:27:11 -0300

    diagrama de classes adicionado

--- lab5/src/saga/controlClientes/ValidaControllerCliente.java
+++ lab5/src/saga/controlClientes/ValidaControllerCliente.java
@@ -29,7 +29,6 @@ public class ValidaControllerCliente {
             throw new IllegalArgumentException(mensagem);
         }
     }
-
     /**
      * Método que serve para validas as entradas do método "cadastraCliente".
      *
